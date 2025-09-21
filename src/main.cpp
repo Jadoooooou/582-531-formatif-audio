adc223e5a6e5c2ee41316375f90acb9be66ea562
Jadoooooou <2366906@cmontmorency.qc.ca>
2025-09-21 12:54:51 -0400

    code_base

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
+#include <FastLED.h>
+#include <MicroOscSlip.h>
+MicroOscSlip<128> monOsc(&Serial);
 
-// put function declarations here:
-int myFunction(int, int);
+#define MA_BROCHE_ANGLE 32
+#define MA_BROCHE_BOUTON 39
+CRGB monPixel;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode( MA_BROCHE_BOUTON , INPUT );
+
+  Serial.begin(115200);
+
+  FastLED.addLeds<WS2812, 27, GRB>(&monPixel, 1);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // oscslip_pd
+  int maLectureBouton = digitalRead( MA_BROCHE_BOUTON );
+  monOsc.sendInt("/bouton", maLectureBouton);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // oscslip_angle
+  int maLectureAnalogique = analogRead(MA_BROCHE_ANGLE);
+  monOsc.sendInt("/angle", maLectureAnalogique);
+  delay(100);
+
+  // couleur_pixel
+  monPixel = CRGB(0, millis() % 25, 0);
+  FastLED.show();
 }
